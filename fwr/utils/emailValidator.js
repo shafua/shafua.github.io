4943c84c9fe0171ffcb36890b4029dab79a38e29
Dmytro Shaforostov <dshaf7@gmail.com>
2017-02-09 19:03:00 +0200

    added forgiving-wizard-react as fwr

---
+++ fwr/utils/emailValidator.js
@@ -0,0 +1,4 @@
+export default function emailValidator(email){
+  let reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return email && (reg.test(email) ? 'ok' : 'wrong')
+}
