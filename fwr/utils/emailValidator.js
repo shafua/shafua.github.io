eda93dea89eb50b03f322024104abd5ff6bd4a66
Dmytro Shaforostov <dshaf7@gmail.com>
2017-02-12 15:17:53 +0200

    fixed fwr bundle

--- fwr/utils/emailValidator.js
+++
@@ -1,4 +0,0 @@
-export default function emailValidator(email){
-  let reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  return email && (reg.test(email) ? 'ok' : 'wrong')
-}
